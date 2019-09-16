e8500ac3a175e78b0dad05f430cdef9cbd69f684
wangyao <wangyao@MacBook-Air.local>
2019-09-16 09:17:21 -0400

    rename entry point to app.py

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, redirect
+#!/usr/bin/env python
+from templates import app, db
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
-if __name__ == '__main__':
-  app.run(port=33507)
+if __name__ == "__main__":
+    #app.debug = True
+    # Because we did not initialize Flask-SQLAlchemy with an application
+    # it will use `current_app` instead.  Since we are not in an application
+    # context right now, we will instead pass in the configured application
+    # into our `create_all` call.
+    #db.create_all(app=app)
+    app.run(port=33507)
